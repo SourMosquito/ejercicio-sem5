dadb7fbb9248c72eb89568c78abbb0a7b1106b08
Mauricio <mau03041@gmail.com>
2021-06-04 19:00:41 -0500

    Agregando estructura clientes modelo, controlador y rutas

---
+++ models/Customers.js
@@ -0,0 +1,40 @@
+const Sequelize = require ('sequelize');
+const db = require('../config/bd');
+
+const Customers = db.define('Estado', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: Sequelize.STRING(35),
+        allowNull: false,
+    },
+    apellidos: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+    },
+    RFC: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+    },
+    Direccion: {
+        
+    },
+    Email: {
+        type: Sequelize.STRING(35),
+        allowNull: false,
+    },
+    Telefono: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+    },
+    Status: {
+        type: Sequelize.BOOLEAN(),
+        allowNull: false,
+    },
+
+});
+
+module.exports = Customers;
